120c6f7b49424a5ce2b97c6b4e39219d223c4fac
Juan José López Delgado <juanjyce@gmail.com>
2013-08-28 00:31:08 -0400

    Added descriptions, moved stuff, finished duprem
    CString moved to right folder
    Finished duplicate character remover

--- CCI/CStringReverse/CString.java
+++ CCI/CStringReverse/CString.java
@@ -1,57 +1,63 @@
-public class CString {
-    public char[] cstring;
+/*
+// C-Style String class with reverse operation
+// @author Juan Jose Lopez Delgado
+// 27-08-13
+*/
 
-    public CString(String cin) {
-	cin += '\n';
-	cstring = cin.toCharArray();
+public class CString {
+  public char[] cstring;
+  
+  public CString(String cin) {
+    cin += '\n';
+    cstring = cin.toCharArray();
+  }
+  
+  public CString(char[] cin) {
+    cstring = cin;
+  }
+  
+  public CString reverse () {
+    int length = 0;
+    while (cstring[length] != '\n') { length++; }
+    char[] charArr = new char[length + 1];
+    
+    for (int i = length - 1; i >= 0; i--) {
+      charArr[length - i - 1] = cstring[i];
     }
     
-    public CString(char[] cin) {
-	cstring = cin;
+    charArr[length] = '\n';
+    return new CString(charArr);
+  }
+  
+  public  void reverseInPlace (CString cin) {
+    int length = 0;
+    char buf;
+    
+    while (cstring[length] != '\n') { length++; }
+    for (int i = 0; i < length /2; i++) {
+      buf = cin.cstring[i];
+      cin.cstring[i] = cin.cstring[length-i-1];
+      cin.cstring[length-i-1] = buf;
     }
-
-    public CString reverse () {
-	int length = 0;
-	while (cstring[length] != '\n') { length++; }
-	char[] charArr = new char[length + 1];
-
-	for (int i = length - 1; i >= 0; i--) {
-	    charArr[length - i - 1] = cstring[i];
-	}
-	
-	charArr[length] = '\n';
-	return new CString(charArr);
+  }
+  
+  public static void main(String[] args) {
+    if (args[0] == null) {
+      System.out.println("Please provide an argument and try again");
+      return;
     }
-
-    public  void reverseInPlace (CString cin) {
-	int length = 0;
-	char buf;
-	
-	while (cstring[length] != '\n') { length++; }
-	for (int i = 0; i < length /2; i++) {
-	    buf = cin.cstring[i];
-	    cin.cstring[i] = cin.cstring[length-i-1];
-	    cin.cstring[length-i-1] = buf;
-	}
+    CString cs = new CString(args[0]);
+    for (char a : cs.cstring) {
+      System.out.print(a);
     }
-
-    public static void main(String[] args) {
-	if (args[0] == null) {
-	    System.out.println("Please provide an argument and try again");
-	    return;
-	}
-	CString cs = new CString(args[0]);
-	for (char a : cs.cstring) {
-	    System.out.print(a);
-	}
-	cs = cs.reverse();
-	for (char a : cs.cstring) {
-	    System.out.print(a);
-	}
-	cs.reverseInPlace(cs);
-	for (char a : cs.cstring) {
-	    System.out.print(a);
-	}
+    cs = cs.reverse();
+    for (char a : cs.cstring) {
+      System.out.print(a);
     }
-
+    cs.reverseInPlace(cs);
+    for (char a : cs.cstring) {
+      System.out.print(a);
+    }
+  }
+  
 }
--- CCI/DuplicateRemover/DupRem.java
+++ CCI/DuplicateRemover/DupRem.java
@@ -1,8 +1,25 @@
+import java.util.Arrays;
+/*
+// Design an algorithm and write code to remove the duplicate characters in a string 
+// without using any additional buffer NOTE: One or two additional variables are fine 
+// An extra copy of the array is not. 
+// @author Juan Jose Lopez Delgado
+// 27-08-13
+*/
+
 public class DupRem {
 
+  public String RemDup(String cin) {
+    if (cin.length() <= 1) { return cin; }
+    char[] charArr = cin.toCharArray();
+    Arrays.sort(charArr);
+    String buf = "" +  charArr[0];
     
-    
-
-
-
+    for (int i = 1; i < charArr.length; i++) {
+      if (charArr[i - 1] != charArr[i]) {
+          buf += charArr[i];
+      }
+    }
+  return buf;
+  }
 }
