29ac64bd47740adb045e08043e7624b6a81555a2
Juan José López Delgado <juanjyce@gmail.com>
2014-01-24 00:11:17 -0500

    Added bitwise string reverse
    for funsies

--- StringWork/Sentence.java
+++ StringWork/Sentence.java
@@ -1,5 +1,6 @@
 /*
 // Reverse a Sentence, by word.
+// Bonus Bitwise string reverse
 // Input: String
 // Output: reversed String
 // @author Juan Jose Lopez Delgado
@@ -11,17 +12,18 @@ public class Sentence {
     if(str == null) { return null; }
    char[] ch = str.toCharArray();
    char temp;
+   int l = ch.length;
    
-   for (int i = 0; i < ch.length/2; i++) {
+   for (int i = 0; i < l/2; i++) {
      temp = ch[i];
-     ch[i] = ch[ch.length-i-1];
-     ch[ch.length-i-1] = temp;
+     ch[i] = ch[l-i-1];
+     ch[l-i-1] = temp;
    }
    System.out.println(ch);
    
    int pos = 0;
-   for(int i = 0; i <= ch.length ; i++) {
-     if (i == ch.length || ch[i] == delim) {
+   for(int i = 0; i <= l ; i++) {
+     if (i == l || ch[i] == delim) {
        for(int j = 0; j < (i - pos)/2; j++) {
          temp = ch[j + pos];
          ch[j + pos] = ch[i - 1 - j];
@@ -33,5 +35,16 @@ public class Sentence {
       System.out.println(ch);
    
   return String.valueOf(ch) ;
+  }
+  
+  public static String reverseBitwise(String str) {
+    char[] ch = str.toCharArray();
+    int l = ch.length;
+    for (int i = 0; i < l/2; i++) {
+      ch[i] = (char) (ch[i] ^ ch[l - 1 - i]);
+      ch[l - 1 - i] = (char) (ch[i] ^ ch[l - 1 - i]);
+      ch[i] = (char) (ch[i] ^ ch[l - 1 - i]);
+    }
+    return String.valueOf(ch);
   }
 }
